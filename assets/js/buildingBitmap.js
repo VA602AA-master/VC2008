af6b1ea4e1f3219be5751c735ba163a608fd5183
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2018-06-13 16:47:21 +0200

    Simplification of the building map
    The creation of the container SVG is moved to the main app, so the same container can be shared by multiple layers `g`

--- assets/js/buildingBitmap.js
+++ assets/js/buildingBitmap.js
@@ -1,33 +1,18 @@
 
 function buildingBitmap(){
-	var width = 910;
-	var height = 610;
-	var svg;
-
-
 	function me(selection){
-		svg = selection
-			.append("svg")
-			.attr("width", width)
-		.attr("height", height);
-	}
 
-	me.data = function(_){
-		if(!arguments.length) return data;
-
-		// data is expected as an array of array
-		//first dimension: create the rows
-		var gr = svg.selectAll("g.row")
-			.data(_)
+		let gr = selection.selectAll("g.row")
+			.data(selection.datum())
 			.enter()
 			.append("g")
 			.classed("row",true)
 		.attr("transform",function(d,i){return "translate(0,"+(i*10)+")"});
 
-		var wallColor = d3.scaleOrdinal()
+		let wallColor = d3.scaleOrdinal()
 			.domain([0,1])
 		.range(["white", "lightgray"]);
-		
+
 		gr.selectAll("rect")
 			.data(function(d){return d})
 			.enter()
@@ -37,21 +22,7 @@ function buildingBitmap(){
 			.attr("height",10)
 			.attr("fill",function(d){return wallColor(d)});
 
-	}
 
-	// setter and getter
-	me.height = function(_){
-		if(!arguments.length) return height;
-		height = _;
-
-		return me;
-	}
-
-	me.width = function(_){
-		if(!arguments.length) return width;
-		width = _;
-
-		return me;
 	}
 
 	return me;
