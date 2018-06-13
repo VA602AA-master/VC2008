e92da3ec8d59f1b11a83e972d6670f11803e467e
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2018-06-13 16:30:17 +0200

    Refactoring using component for main app

--- assets/js/building.js
+++ assets/js/building.js
@@ -1,17 +1,59 @@
-d3.text("assets/data/building_map.txt")
-  .then(function(map) {
-    let aMap = map.split('\n').map(function(d) {
-      return d.trim().split(' ').map(function(e) {
-        return +e
+function BuildingApp() {
+  var svg;
+  var width = 910;
+  var height = 610;
+  var trajectories;
+  var timeline;
+
+  function me(selection) {
+    console.log("BuildingApp");
+    d3.text("assets/data/building_map.txt")
+      .then(function(map) {
+        let aMap = map.split('\n').map(function(d) {
+          return d.trim().split(' ').map(function(e) {
+            return +e
+          })
+        });
+        console.log('map', aMap);
+        let building = buildingBitmap();
+
+        d3.select("#building").call(building);
+        building.data(aMap);
+
       })
-    });
-    console.log('map', aMap);
-    let building = buildingBitmap();
+      .catch(function(error) {
+        console.log(error);
+      });
 
-    d3.select("#building").call(building);
-    building.data(aMap);
 
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
+    d3.tsv("assets/data/rfid_assignments.txt")
+      .then(rows => {
+        let ids = rows
+          .filter(row => {
+            let entries = d3.values(row);
+            return (entries[0].length > 0) // ignore rows with invalid id (not numeric)
+          })
+          .map(row => {
+            let entries = d3.values(row);
+            return {
+              id: +entries[0],
+              person: entries[1]
+            }
+          });
+        console.log("ids", ids);
+
+        var rlist = rfidList();
+        d3.select("#persons")
+          .datum(ids)
+          .call(rlist);
+      });
+  }
+
+
+  return me;
+}
+
+
+var buildingApp = BuildingApp();
+d3.select("#main")
+  .call(buildingApp);
