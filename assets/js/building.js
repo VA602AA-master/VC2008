70136792c532cd75257e561035de47edcb903c35
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2018-06-13 16:49:38 +0200

    Visualization of trajectories path
    Each trajectory is visualized in its 837 time stamps. A variable allows to modify the temporal extent

--- assets/js/building.js
+++ assets/js/building.js
@@ -17,8 +17,15 @@ function BuildingApp() {
         console.log('map', aMap);
         let building = buildingBitmap();
 
-        d3.select("#building").call(building);
-        building.data(aMap);
+        svg = d3.select("#building")
+  				.append("svg")
+  				.attr("width", width)
+  			.attr("height", height);
+
+			svg.append("g")
+				.classed("buildingMap", true)
+				.datum(aMap)
+				.call(building);
 
       })
       .catch(function(error) {
@@ -46,8 +53,62 @@ function BuildingApp() {
   				.datum(ids)
   			.call(me.personList);
       });
-  }
 
+    d3.tsv("assets/data/rfid_pathway.txt")
+    .then(rows => {
+      let paths = rows.map(row => {
+        return {
+          person: +row.Person,
+          time: +row.Time,
+          x: +row.xcor,
+          y: +row.ycor
+        }
+      });
+      var trajs = d3.nest()
+        .key(function(d){return d.person})
+      .entries(paths);
+
+      var trs = d3.values(trajs).map(function(d){
+        var pl = d.values.map(function(p,i){
+          if(i==0) return 0;
+          return euclideanDistance(p, d.values[i-1])
+        });
+        return {
+        person: +d.key,
+        values: d.values.map(function(p){return {x:p.x, y:p.y}}),
+        path_length: pl.reduce(function(a,b){return a+b}, 0),
+        delta_s: pl
+      }});
+
+      console.log("trajs",trajs);
+      console.log("trs",trs);
+
+      trajectories = Trajectories();
+      svg.append("g")
+        .classed("trajs",true)
+        .datum(trs)
+      .call(trajectories);
+
+      // timeline = TimelineBrush().domain([0,trs[0].values.length]);
+      // d3.select("#timeline")
+      // .call(timeline);
+
+      // d3.select("#status")
+      // .call(me.statusbar);
+    });
+
+  }
+  function euclideanDistance(a,b){
+		return Math.sqrt(Math.pow(a.x -b.x,2) + Math.pow(a.y - b.y,2));
+	}
+
+	function distances(p1,p2){
+		console.log("p1",p1);
+		console.log("p2",p2);
+		return p1.values.map(function(d,i){
+			return [i, euclideanDistance(d, p2.values[i])]
+			});
+	}
 
   return me;
 }
